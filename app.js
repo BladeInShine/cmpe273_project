2db1416b99553d71b14f54bddc259c40ea9bf697
yin <yinfeiyu43@gmail.com>
2014-11-23 15:41:30 -0800

    Modify app.js, conn.js

--- app.js
+++ app.js
@@ -1,35 +1,103 @@
-
 /**
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var passport = require('passport');
 
+var user = require('./routes/user');
+//var cat = require('./routes/cat');
+//var product = require('./routes/product');
+var auction = require('./routes/auction');
+//var selling = require('./routes/selling');
+//var review = require('./routes/review');
+
+var con = require('./conn');
+var passportlocal = require('passport-local');
+var bodyParser= require('body-parser');
+var cookieParser =require('cookie-parser');
+var expressSession = require('express-session');
 var app = express();
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(cookieParser());
+app.use(expressSession({
+	secret: process.env.SESSION_SECRET||'secret',
+    resave: false,
+    saveUninitialized : false
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+}));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+app.use(passport.initialize());
+app.use(passport.session());
+ 
+passport.use(new passportlocal.Strategy(user.passportAauth));
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+passport.serializeUser(user.serializeUser);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+passport.deserializeUser(user.deserializeUser);
+
+
+app.get('/', user.root);
+
+app.get('/login', user.login);
+
+app.get('/loginfail', user.loginfail);
+
+app.post('/login',passport.authenticate('local', {failureRedirect: '/loginfail'}), user.loginPost);
+
+app.get('/logout', user.logout);
+
+app.get('/signup', user.signup);
+
+app.get('/signupfail', user.signupfail);
+
+app.post('/signup', user.signupPost);
+
+
+app.get('/profile', user.profile);
+
+app.get('/seller', user.getAllSeller);
+
+app.get('/customer', user.getAllCustomer);
+
+
+app.get('/cat/', cat.getAllCat);
+
+app.get('/cat/:catname', cat.getCat);
+
+app.post('/cat', cat.createCat);
+
+app.post('/deletecats', cat.deleteCat);
+
+app.get('/product', product.getAllProduct);
+
+app.get('/product/:productname', product.getProduct);
+
+app.post('/product', product.createProduct);
+
+app.post('/deleteproduct', product.deleteProduct);
+
+app.post('/buyproduct', product.buyProduct);
+
+
+app.get('/auction', auction.getAllAction);
+
+app.post('/bidproduct', auction.bidProduct);
+
+
+app.get('/createreview/:productid', review.getReviewPage);
+
+app.post('/createreview', review.createReview);
+
+app.get('/review', review.getReview);
+
+
+app.get('');
+
+ 
+var port= process.env.PORT||3000;
+app.listen(port,function(){
+	console.log('http://127.0.0.1:'+port+'/');
+  
+  });
--- conn.js
+++ conn.js
@@ -1,1 +1,108 @@
-aaaa
+/**
+ * New node file
+ */
+
+var mysql = require('mysql');
+
+var av = [true, true, true, true, true, true, true, true, true, true];
+
+var cons = [];
+
+function getCon(){
+	
+	if(cons.length === 0){
+		
+		console.log("Init connection pool.");
+		for(var j = 0; j < 10; j ++){
+			
+			cons[j] = mysql.createConnection({
+				host : 'localhost',
+				user : 'root',
+				password : '12345678',
+				port : '3306',
+				database : 'test'
+				});
+		}
+	}
+	
+	while(true){
+		for(var i = 0; i < 10; i ++){
+		
+			if(av[i] === true){
+			
+				av[i] = false;
+				console.log(i + "th connection is leased.");
+				var result = [cons[i], i];
+				return result;
+			}
+		}
+	}
+}
+
+function returnCon(i){
+	
+	console.log(i + "th connection is returned.");
+	av[i] = true;
+}
+
+var connection = mysql.createConnection({
+	host : 'localhost',
+	user : 'root',
+	password : '12345678',
+	port : '3306',
+	database : 'test'
+	});
+
+
+function fetchData(sqlQuery, callback){
+	
+	console.log("\nSQL Query::"+sqlQuery);
+	
+	var conn = getCon();
+	
+	var i = conn[1];
+	
+	conn[0].query(sqlQuery, function(err, rows, fields) {
+		if(err){
+			console.log("ERROR: " + err.message);
+		}
+		else
+		{
+			callback(err, rows);
+		}
+	});
+	
+	returnCon(i);
+}
+
+function insert(qS){
+	
+	var conn = getCon();
+	
+	var i = conn[1];
+	
+	console.log("\nSQL Query::" + qS);
+	conn[0].query(qS);
+	returnCon(i);
+}
+
+
+function query(queryS, con){
+
+	con.query(queryS, function (err, rows, fields) {
+		if (err) throw err;
+		if(rows.length !== 0){
+			
+			console.log("DATA: " + rows[0].password);
+		}
+		else{console.log("Empty data");}
+	});
+}
+
+
+exports.query = query;
+exports.connection = connection;
+exports.fetchData = fetchData;
+exports.insert = insert;
+exports.getCon = getCon;
+exports.returnCon = returnCon;
