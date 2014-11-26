c5a0e83938341c5dcb5e86792c267061c638cc6e
Chen <Chen@Icesnowbeauty>
2014-11-25 23:25:27 -0800

    Add declarations

--- routes/cat.js
+++ routes/cat.js
@@ -1,3 +1,77 @@
 /**
  * New node file
  */
+/**
+ * New node file
+ */
+
+var ejs = require("ejs");
+var mysql = require('../conn');
+
+
+function getAllCat(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+function getCat(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+function createCat(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+function deleteCat(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+exports.getAllCat=getAllCat;
+exports.getCat=getCat;
+exports.createCat=createCat;
+exports.deleteCat=deleteCat;
+
+
--- routes/product.js
+++ routes/product.js
@@ -1,3 +1,91 @@
 /**
  * New node file
  */
+/**
+ * New node file
+ */
+var ejs = require("ejs");
+var mysql = require('../conn');
+
+
+function getAllProduct(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+function getProduct(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+function createProduct(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+function deleteProduct(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+function buyProduct(req,res) {
+
+	ejs.renderFile('./views/signin.ejs',function(err, result) {
+	   // render on success
+	   if (!err) {
+	            res.end(result);
+	   }
+	   // render or error
+	   else {
+	            res.end('An error occurred');
+	            console.log(err);
+	   }
+   });
+}
+
+exports.getAllProduct=getAllProduct;
+exports.getProduct=getProduct;
+exports.createProduct=createProduct;
+exports.deleteProduct=deleteProduct;
+exports.buyProduct=buyProduct;
+
+
