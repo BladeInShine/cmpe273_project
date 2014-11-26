80f630250138e3f4d119cc01727fa242591a46c1
BladeInShine <dengdechen@gmail.com>
2014-11-25 23:57:34 -0800

    empty frame done for other test

--- routes/review.js
+++ routes/review.js
@@ -1,3 +1,12 @@
-/**
- * New node file
- */
+var sql_con = require('../conn');
+
+function getReviewPage(req, res){}
+function createReview(req, res){}
+function getReview(req, res){}
+function deleteReview(req, res){}
+
+
+exports.getReviewPage = getReviewPage;
+exports.getReviewPage = createReview;
+exports.getReviewPage = getReview;
+exports.getReviewPage = deleteReview;
