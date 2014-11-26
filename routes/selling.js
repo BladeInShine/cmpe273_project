4bb1fecdcb2a3a7f7a50db4bc57b8c36787c6a06
zhonggn <zhonggn@10.189.33.155>
2014-11-25 22:34:49 -0800

    add functions in sellings

--- routes/selling.js
+++ routes/selling.js
@@ -1,1 +1,24 @@
-asdf
+var sql_con = require('../conn');
+function getAllSelling(req, res){
+	
+	var qS = "select * from `cmpe273project`.`selling`;";
+
+	sql_con.fetchData(qS, function(error, rows){
+		
+		res.render('index',{data: rows});
+	});
+}
+
+function createSelling(req, res){}
+
+function deleteSelling(req, res){}
+
+function buyProduct(req,res){}
+
+function getSelling(req,res){}
+
+exports.getAllSelling = getAllSelling;
+exports.createSelling = createSelling;
+exports.deleteSelling = deleteSelling;
+exports.buyProduct = buyProduct;
+exports.getSelling = getSelling;
