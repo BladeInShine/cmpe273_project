db86bb486ef6f1d815d8fc645bba207601aed6f7
Chen <Chen@Icesnowbeauty>
2014-11-30 12:32:45 -0800

    implement the getAllCat and the page to show the cat

--- routes/cat.js
+++ routes/cat.js
@@ -1,72 +1,54 @@
-/**
- * New node file
- */
-/**
- * New node file
- */
-
-var ejs = require("ejs");
-var mysql = require('../conn');
+var ejs = require('ejs');
+var sql = require('../conn');
+var connection= require('../conn');
+var passpport=require('passport');
 
 
 function getAllCat(req,res) {
-
-	ejs.renderFile('./views/signin.ejs',function(err, result) {
-	   // render on success
-	   if (!err) {
-	            res.end(result);
-	   }
-	   // render or error
-	   else {
-	            res.end('An error occurred');
-	            console.log(err);
-	   }
-   });
+	
+	var query= "select * from cat";
+	var cat ='';
+	console.log("get all function");
+	
+	sql.fetchData(query, function(error, result){
+		for(var i=0; i < result.length; i++){
+			cat +='<option value="'+result[i].catname+'">'+result[i].catname+'</option>';
+		}
+		console.log(cat);
+		res.render('test',{isAuthenticate:req.isAuthenticated(),
+			catopt: cat,
+			});
+	});
 }
 
 function getCat(req,res) {
 
-	ejs.renderFile('./views/signin.ejs',function(err, result) {
-	   // render on success
-	   if (!err) {
-	            res.end(result);
-	   }
-	   // render or error
-	   else {
-	            res.end('An error occurred');
-	            console.log(err);
-	   }
-   });
 }
 
 function createCat(req,res) {
-
-	ejs.renderFile('./views/signin.ejs',function(err, result) {
-	   // render on success
-	   if (!err) {
-	            res.end(result);
-	   }
-	   // render or error
-	   else {
-	            res.end('An error occurred');
-	            console.log(err);
-	   }
-   });
+	console.log("create cat");
+	var name = req.query.catName;
+	var insertSQL = "INSERT INTO cat (catname) VALUES('"+name+"')";
+	var selectSQL = "select * from cat where UPPER(categoryname) = UPPER('"+name+"')";
+	console.log("create cat");
+	
+	sql.fetchData(selectSQL, function(error, result){
+		sql.fetchData(insertSQL, function(error, result){
+			res.redirect('/test');	
+		});
+	});	
 }
 
 function deleteCat(req,res) {
-
-	ejs.renderFile('./views/signin.ejs',function(err, result) {
-	   // render on success
-	   if (!err) {
-	            res.end(result);
-	   }
-	   // render or error
-	   else {
-	            res.end('An error occurred');
-	            console.log(err);
-	   }
-   });
+	var name = req.query.catName;
+	var selectSQL = "select * from cat where UPPER(catname) = UPPER('"+name+"')";
+	var deleteSQL = "DELETE FROM cat WHERE catname = '"+name+"'";
+	sql.fetchData(selectSQL, function(error, result){
+		sql.fetchData(deleteSQL, function(error, result){
+			res.redirect('/test');	
+		});
+	});	
+	
 }
 
 exports.getAllCat=getAllCat;
