f76a5925fa9c493c1fd28d4dca80c8d38b5f0c63
zhonggn <zhonggn@192.168.0.2>
2014-11-30 16:04:02 -0800

    add selling.createSellingPage and auctioncreation.ejs

--- routes/selling.js
+++ routes/selling.js
@@ -9,7 +9,62 @@ function getAllSelling(req, res){
 	});
 }
 
-function createSelling(req, res){}
+function createSelling(req, res){
+	//if(!req.isAuthenticated()){res.redirect('/login');}
+	console.log("Inside createSelling");
+
+	console.log(req.files);
+	
+//	fs.readFile(req.files.image.path, function (err, data) {
+//		  // ...
+//		  var newPath = __dirname + "/images/";
+//		  fs.writeFile(newPath, data, function (err) {
+//		  });
+//		});
+
+	var len = req.files.image.path.length;
+	var pictureUrl = req.files.image.path.substring(42,len);
+	var name = req.body.productname; 
+	var des = req.body.des;
+	var owner = "a";
+	var cat = req.body.cat;
+	var condition = req.body.condition;
+	var quantity = req.body.quantity;
+	var soa = "sell";
+	
+	var qS = "INSERT INTO `cmpe273project`.`product` (`name`, `description`, `owner`, `cat`, `pictureurl`, `sellorauction`, `condi`) VALUES ('" + name + "', '" + des + "', '" + owner + "', '" + cat + "', '" + pictureUrl + "', '" + soa + "', '" + condition + "');";
+	sql_con.insert(qS);
+	
+	var qS2 = "SELECT * FROM `cmpe273project`.`product` WHERE name = '" + name + "' and description = '" + des + "' and owner = '" + owner + "' and cat = '" + cat + "' and pictureurl = '" + pictureUrl + "' and sellorauction = '" + soa + "' and condi = '" + condition + "';";
+	
+	sql_con.fetchData(qS2, function(error, rows){
+		
+		console.log("rows size: " + rows.length);
+		var productId = parseInt(rows[0].id);
+		console.log("Id is " + productId);
+		
+		var dateObj = new Date();
+		var month = dateObj.getUTCMonth() + 1; //months from 1-12
+		var day = dateObj.getUTCDate();
+		var year = dateObj.getUTCFullYear();
+		var date = month + "/" + day + "/" + year;
+		
+		var startDate = date;
+		var seller = "a";
+		//var startPrice = req.body.startprice;
+		var addPrice = req.body.addprice;
+		var inProgress = "true";
+		
+		var qS = "INSERT INTO `cmpe273project`.`selling` (`product`, `price`,`startdate`, `quantity`) VALUES ('" + productId + "', '" + addPrice + "','" + startDate + "', '" + quantity + "');";
+
+		sql_con.insert(qS);
+			
+		res.render('auction',{email : "a", productname: name, currentprice: startPrice, condition: condition, addprice: addPrice, bidnum: 0, pictureurl: pictureUrl});
+		//res.render('auction');
+	});
+	
+
+}
 
 function deleteSelling(req, res){}
 
@@ -17,8 +72,25 @@ function buyProduct(req,res){}
 
 function getSelling(req,res){}
 
+function createSellingPage(req,res){
+	if(false){res.redirect('/login');}
+	
+		else{
+			
+			var qS = "SELECT * FROM cmpe273project.cat;";
+		
+			sql_con.fetchData(qS, function(error, rows){
+				
+				res.render('sellingcreation', {cats: rows});
+				
+			});
+	
+		}
+}
+
 exports.getAllSelling = getAllSelling;
 exports.createSelling = createSelling;
 exports.deleteSelling = deleteSelling;
 exports.buyProduct = buyProduct;
 exports.getSelling = getSelling;
+exports.createSellingPage = createSellingPage;
